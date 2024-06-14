19fb1bcfe443d038c9e673be22b703e0e9e0ad1b
Victor <vzagulin@gmail.com>
2024-06-14 14:42:46 +0300

    boards are movable and fully bounded with base

--- TaskManager/settings.py
+++ TaskManager/settings.py
@@ -54,6 +54,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
 ROOT_URLCONF = 'TaskManager.urls'
 
 TEMPLATES = [
--- api/views.py
+++ api/views.py
@@ -1,9 +1,15 @@
 from rest_framework.viewsets import ModelViewSet
 from api.serializers import TaskSerializer
 from tasks.models import Task
+from rest_framework.permissions import IsAuthenticated
 
 
 class TaskViewSet(ModelViewSet):
+
+    permission_classes = [IsAuthenticated]
+
     queryset = Task.objects.get_queryset()
     serializer_class = TaskSerializer
-    
+
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
